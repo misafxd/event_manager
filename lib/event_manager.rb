fe6fad43e0b3321d1a5dee531ea5d2e71e44fb89
misafxd <pisichu.mafs@gmail.com>
2023-11-15 17:28:13 -0600

    Add clean phone numbers function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
-template_letter = File.read('form_letter.html')
+require 'erb'
 
 puts 'Event Manager Initialized!'
 
@@ -13,27 +13,54 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(',')
+    ).officials
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+def clean_phone(phone)
+  digits = phone.scan(/\d/).join
+  if digits.size == 11 && digits[0] == '1'
+    digits[1..10]
+  elsif digits.size == 10
+    digits
+  else
+    "Wrong number"
+  end
+end
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol)
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
-  
-  puts personal_letter
+  # form_letter = erb_template.result(binding)
+  # save_thank_you_letter(id, form_letter)
+
+  phone = clean_phone(row[:homephone])
+  puts phone
 end
