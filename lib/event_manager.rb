eed5385b830a936b42de6e82baaf5359e9afafac
misafxd <pisichu.mafs@gmail.com>
2023-11-17 12:34:46 -0600

    Add methods to clean an print registration days

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 puts 'Event Manager Initialized!'
 
@@ -44,6 +45,22 @@ def clean_phone(phone)
   end
 end
 
+def clean_date(date)
+  begin
+    Date.strptime(date, '%m/%d/%y %H:%M')
+  rescue StandardError
+    'Invalid Date Format'
+  end
+end
+
+def print_register_day_count(register_count)
+  sorted_register_count = register_count.sort_by { |_, value| -value }.to_h
+  puts 'Registration Day List'
+  sorted_register_count.each do |element|
+    puts "On #{element[0]}, #{element[1]} people registered"
+  end
+end
+
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
@@ -52,15 +69,20 @@ contents = CSV.open(
   headers: true,
   header_converters: :symbol)
 
+register_count = Hash.new(0)
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  # form_letter = erb_template.result(binding)
-  # save_thank_you_letter(id, form_letter)
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
 
   phone = clean_phone(row[:homephone])
-  puts phone
+
+  register_date = clean_date(row[:regdate])
+  register_count[register_date.strftime('%A')] += 1
 end
+
+print_register_day_count(register_count)
